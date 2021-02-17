d4442dc4bfd0cf2eff51b5df4fd7352ec7148682
Amaury Diaz <amauryuh@gmail.com>
2021-02-17 17:09:25 -0500

    add server configuration to the client project.

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const uri = process.env.SERVER_ADDRESS || `https://localhost/graphql`;
+export const uri =
+  process.env.SERVER_ADDRESS || `https://195.181.247.138/graphql`;
 
 export const keycloakServer =
-  process.env.KEYCLOAK_REDIRECT || "https://localhost/auth";
+  process.env.KEYCLOAK_REDIRECT || "https://195.181.247.138/auth";
 
 export const keycloakRealm = process.env.KEYCLOAK_REALM || "TraderAdmin";
 
