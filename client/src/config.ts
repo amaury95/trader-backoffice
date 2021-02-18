11257971b9316a5e1ec37d62e648017cb99f7dfe
Amaury Diaz <amauryuh@gmail.com>
2021-02-18 17:31:22 -0500

    new server setup.

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
 export const uri =
-  process.env.REACT_APP_SERVER_ADDRESS ?? `http://195.181.247.138/graphql`;
+  process.env.REACT_APP_SERVER_ADDRESS ?? `http://80.209.237.197/graphql`;
 
 export const keycloakServer =
-  process.env.REACT_APP_KEYCLOAK_REDIRECT ?? "http://195.181.247.138/auth";
+  process.env.REACT_APP_KEYCLOAK_REDIRECT ?? "http://80.209.237.197/auth";
 
 export const keycloakRealm =
   process.env.REACT_APP_KEYCLOAK_REALM ?? "TraderAdmin";
